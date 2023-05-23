1003904d2ed33d4ab9ec11b2cb79ac968982663e
Polina <p_karavayeva@mail.ru>
2023-05-24 00:57:54 +0300

    Добавьте файлы проекта.

---
+++ domashka/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace domashka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трёхзначное число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int secondDigit = (num / 10) % 10;
+
+            Console.WriteLine("Вторая цифра числа {0} равна {1}", num, secondDigit);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+        
+    
+
